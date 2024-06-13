ef7e705f96f4fea909946e33e4f4742e08708953
unknown <dev23.mxpertz@gmail.com>
2024-06-13 18:45:53 +0530

    api added

--- src/helper/axiosconfig.js
+++ src/helper/axiosconfig.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "https://brainylingo.azurewebsites.net/api/",
-  baseURL: "http://localhost:9000/api/",
-  // baseURL: "https://brainlybackend-june-13.onrender.com/api/",
+  // baseURL: "http://localhost:9000/api/",
+  baseURL: "https://brainlybackend-june-13.onrender.com/api/",
   withCredentials: true,
 });
 
